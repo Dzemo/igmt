3a92e1165cb013a04fb86ed6502563851463431e
Gaboriaud <Gaboriaud@192.168.1.12>
2015-04-05 01:20:09 +0200

    * Fix Connection VM Prod

--- database_info.php
+++ database_info.php
@@ -1,7 +1,24 @@
+
 <?php
-	$dbhost="localhost";
-	$dbuser="";
-	$dbpass="";
-	$dbname="igmt";
-	$dbparams = array();
+
+$isDev = strpos($_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'],'development');
+
+if($isDev === FALSE ){
+    // Nous sommes en Prod
+    $dbhost="localhost";
+    $dbuser="root";
+    $dbpass="842w[AGUM~QWrR";
+    $dbname="igmt";
+    $dbparams = array();
+}
+else {
+    // Nous sommes en Dev
+    $dbhost="localhost";
+    $dbuser="root";
+    $dbpass="";
+    $dbname="igmt";
+    $dbparams = array(); 
+}
+
+
 ?>
--- script_cost.sql
+++ script_cost.sql
@@ -8,4 +8,4 @@ CREATE TABLE igmt_cost (
 	CONSTRAINT un_igmt_cost UNIQUE (element_from_id, element_to_pay_id),
 	CONSTRAINT fk_element_from FOREIGN KEY (element_from_id) REFERENCES igmt_element(id),
 	CONSTRAINT fk_igmt_element_to_pay FOREIGN KEY (element_to_pay_id) REFERENCES igmt_element(id)
-);
+) ENGINE = MYISAM;
